a45638769bc4d65356e6550ff4ee5e9680a0473b
ashur-k <ashurkanwal@yahoo.com>
2021-06-26 13:35:25 +0100

    solve metup info page footer issue

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,15 +12,15 @@ function MeetupDetails(props) {
                     name="description" 
                     content={props.meetupData.description}
                 />
-            </Head>
+            </Head>        
             <hr />
             <h2 style={{textAlign:'center', color:'#77002e'}}>{props.meetupData.title}</h2>
             <hr/>
-            <MeetupDetail 
+            <MeetupDetail
                 image={props.meetupData.image}
                 address={props.meetupData.address}
                 description={props.meetupData.description}
-            />
+            />         
         </Fragment>
         
     );
